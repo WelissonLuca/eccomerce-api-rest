1c664a94fab622d3dfe05c02d54a08456986d504
Welisson <welissonluca17@gmail.com>
2021-07-23 19:30:03 -0300

    feat: add user router

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import { CreateUserController } from '@modules/accounts/useCases/createUser/createUserController';
+
 const userRouter = Router();
+
+const createUserController = new CreateUserController();
+
+userRouter.post(
+  '/users',
+
+  createUserController.handle
+);
 
 export { userRouter };
