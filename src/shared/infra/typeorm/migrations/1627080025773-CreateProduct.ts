618b0ba8d2899907f49f9b8cea49634352b1ccd1
Welisson <welissonluca17@gmail.com>
2021-07-23 19:43:45 -0300

    feat: add create products migration

---
+++ src/shared/infra/typeorm/migrations/1627080025773-CreateProduct.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProduct1627080025773 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+          },
+          {
+            name: 'size',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
