5dde10fe012b58e9d07d4d867cd0a1edd4566f2e
Welisson <welissonluca17@gmail.com>
2021-07-23 20:23:32 -0300

    feat: add product entity

---
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -0,0 +1,42 @@
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('orders')
+class Product {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  note: string;
+
+  @Column()
+  color: string;
+
+  @Column()
+  size: string;
+
+  @Column()
+  value: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    this.id = this.id ?? uuid();
+  }
+}
+
+export { Product };
