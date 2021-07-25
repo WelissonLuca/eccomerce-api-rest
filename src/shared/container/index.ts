fcd1020754d76cbf5fb25d0398d8422d14f8951a
Welisson <welissonluca17@gmail.com>
2021-07-25 17:15:19 -0300

    feat: add register singleton container product

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,5 +2,11 @@ import { container } from 'tsyringe';
 
 import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository';
 import { IUserRepository } from '@modules/accounts/repositories/IUserRepository';
+import { ProductRepository } from '@modules/products/infra/typeorm/repositories/ProductRepository';
+import { IProductRepository } from '@modules/products/repositories/IProductRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<IProductRepository>(
+  'ProductRepository',
+  ProductRepository
+);
