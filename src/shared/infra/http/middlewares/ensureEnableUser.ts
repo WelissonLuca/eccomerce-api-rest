95ffc603c48b407a812800139e4b17e7fedbcef9
Welisson <welissonluca17@gmail.com>
2021-07-26 00:20:53 -0300

    feat: add ensure enable user and authenticate

---
+++ src/shared/infra/http/middlewares/ensureEnableUser.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { container } from 'tsyringe';
+
+import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository';
+
+export async function ensureEnableUser(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { user_id } = request;
+
+  const usersRepositories = container.resolve(UserRepository);
+
+  const { enable } = await usersRepositories.findById(user_id);
+
+  if (enable) {
+    return next();
+  }
+
+  return response.status(401).json({
+    error: 'Unauthorized, only enable users',
+  });
+}
